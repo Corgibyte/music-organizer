26bda7cd44107d69e673809b5e1df387c8dd1931
Hannah Young <hannah@corgibyte.com>
2021-12-16 09:34:39 -0800

    Add boilerplate code
    Co-authored-by : Kate Kiatsiri <keidsiri@gmail.com>

---
+++ MusicOrganizer/Program.cs
@@ -0,0 +1,20 @@
+using System.IO;   // This is a boilerplate same with every project
+using Microsoft.AspNetCore.Hosting;
+
+namespace MusicOrganizer
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
